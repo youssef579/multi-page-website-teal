b7c26b1af7c68c4175f3d3d6577d1030da6e73f1
Youssef Ragaey <youssefragaa3@gmail.com>
2023-01-28 19:10:03 +0200

    Finish about page

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const menu = document.querySelector(".menu");
 const close = document.querySelector(".close");
 const nav = document.querySelector("nav");
+const boxes = document.querySelector(".directors");
 
 menu.onclick = () => nav.classList.add("clicked");
 close.onclick = () => nav.classList.remove("clicked");
+
+if (boxes != null) {
+    boxes.querySelectorAll("button").forEach((btn, index) => {
+        btn.onclick = function () {
+            btn.parentElement.parentElement.style.setProperty("transform", `rotateY(${(index % 2 == 0)? -180 : 0}deg)`);
+        };
+    });
+}
